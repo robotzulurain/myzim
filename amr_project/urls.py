0cd7d9625ca84ed214a9e586ba401a1c740a1493
Harry Thabo Masuku <biomasuku@gmail.com>
2025-09-29 11:21:44 +0200

    Fix API URLs for Render deployment

--- amr_project/urls.py
+++ amr_project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('amr_reports.urls')),
+    path('api/', include('amr_reports.urls')),  # include the app API URLs
 ]
